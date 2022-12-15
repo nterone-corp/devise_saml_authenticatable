e103593e01a50b553b98d000dab120f15764fbe6
Dan Mlodecki <dmlodecki@gmail.com>
2022-12-15 18:12:34 -0400

    user validity check

--- lib/devise_saml_authenticatable.rb
+++ lib/devise_saml_authenticatable.rb
@@ -113,6 +113,7 @@ module Devise
   # See saml_update_resource_hook for more information.
   mattr_reader :saml_default_update_resource_hook
   @@saml_default_update_resource_hook = Proc.new do |user, saml_response, auth_value|
+    puts "*** resource hook called"
     saml_response.attributes.resource_keys.each do |key|
       user.send "#{key}=", saml_response.attribute_value_by_resource_key(key)
     end
